32121d69370f4cc4fd8f26131a31c8c312fe0487
Your Name <you@example.com>
2025-04-16 11:33:06 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -42,10 +42,10 @@ int is_equal(void* key1, void* key2){
 void insertMap(HashMap * map, char * key, void * value) {
     Pair* nuevoDato = createPair(key, value) ;
     long posicion = hash(key, map->capacity) ;
-    if (map->buckets[posicion]->key != NULL || map->buckets[posicion]->value) {
-        do {
-            posicion++ ;
-        } while (map->buckets[posicion]->key != NULL) ;
+    if (map->buckets[posicion]->key != NULL) {
+        while (map->buckets[posicion]->key != NULL) {
+            posicion += 1 ;
+        }
     }
 
     map->buckets[posicion] = nuevoDato ;
